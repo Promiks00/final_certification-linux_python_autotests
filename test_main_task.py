58fd28021bac1891df8a8ab510cceed5be6c9abb
user <prodemix@gmail.com>
2025-01-10 03:43:23 +0300

    added logging.info ("Test {number} completed") at the end of every test

--- test_main_task.py
+++ test_main_task.py
@@ -19,6 +19,7 @@ class TestNegative:
         testpage.click_login_button()
         error_text = testpage.get_error_text()
         assert error_text == "401"
+        logging.info("Test 1 Completed")
 
 
 class TestPositive:
@@ -30,6 +31,7 @@ class TestPositive:
         testpage.click_login_button()
         user_text = testpage.get_user_text()
         assert user_text == f"Hello, {name}"
+        logging.info("Test 2 Completed")
 
     def test_step3_click_about_link(self, browser):
         logging.info("Test3 Starting")
@@ -37,9 +39,11 @@ class TestPositive:
         testpage.click_about_link()
         about_text = testpage.get_about_text()
         assert about_text == "About Page"
+        logging.info("Test 3 Completed")
 
     def test_step4_get_fontsize_value_of_title_on_page_About(self,browser):
         logging.info("Test4 Starting")
         testpage = OperationsHelper(browser)
         value = testpage.get_property_value_title()
         assert value == "32px", f"Expected font-size value of title on page 'About' as '32px', but got '{value}'"
+        logging.info("Test 4 Completed")
