bb66d918136e83c0f50506db049cf70f866803d0
user <prodemix@gmail.com>
2025-01-10 06:42:34 +0300

    minor logging changes in conftest.py of main_task

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import logging
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -5,8 +6,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="session")
 def browser():
+    logging.info("Setting up Chrome browser...")
     service = Service(executable_path=ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=service, options=options)
+    logging.info("Chrome browser started successfully.")
     yield driver
+    logging.info("Closing the Chrome browser...")
     driver.quit()
+    logging.info("Chrome browser closed.")
